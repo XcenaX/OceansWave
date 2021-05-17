48f8bf8f78befe3dda3aaa1654db986f05b76453
XcenaX <vlad-057@mail.ru>
2021-05-17 16:03:35 +0600

    telegram bot

---
+++ oceanswave/__init__.py
@@ -0,0 +1,5 @@
+from start_bot import TELEGRAM_BOT
+def startup():
+    TELEGRAM_BOT.start_bot()
+
+startup()
--- start_bot.py
+++ start_bot.py
@@ -2,4 +2,3 @@ from Bot import Bot
 from oceanswave.settings import TELEGRAM
 
 TELEGRAM_BOT = Bot(TELEGRAM)
-TELEGRAM_BOT.start_bot()
