cd0cc8e445927c5daf18ecfbbb2591f522ee49f1
XcenaX <vlad-057@mail.ru>
2021-05-17 15:32:58 +0600

    telegram

--- start_bot.py
+++ start_bot.py
@@ -21,9 +21,9 @@ class Bot():
             if self.check_owner(message.from_user.id): 
                 if message.text == "/start":
                     self.add_user(message.from_user.id)
-                    self.bot.send_message(message.from_user.id, "Хай", reply_markup=self.get_main_keyboard())   
+                    self.bot.send_message(message.from_user.id, "Привет", reply_markup=self.get_main_keyboard())   
                 else:
-                    self.bot.send_message(message.from_user.id, "Хай", reply_markup=self.get_main_keyboard())   
+                    self.bot.send_message(message.from_user.id, "Привет", reply_markup=self.get_main_keyboard())   
                     
             else:
                 self.bot.send_message(message.from_user.id, "Я не могу отвечать на сообщения!\nЯ могу только рассылать новости!")
@@ -33,8 +33,8 @@ class Bot():
             if call.data == "add_new": 
                 self.bot.send_message(call.message.chat.id, "Напиши текст рассылки: ")   
                 self.bot.register_next_step_handler(call.message, self.make_mailing)
-            elif call.data == "say_hi":
-                self.bot.send_message(call.message.chat.id, "IJFOWEJFNWEFONWEOFN WEOF NWOENF WONF OWENF WONEF OWINE FWOENIW OIN")
+            else:
+                self.bot.send_message(call.message.chat.id, "Я тебя не понимаю")   
 
     def get_owners(self):
         with open(self.owners_file) as f:
@@ -73,16 +73,11 @@ class Bot():
     def get_main_keyboard(self):
         keyboard = types.InlineKeyboardMarkup() 
         add_new = types.InlineKeyboardButton(text="Сделать рассылку", callback_data="add_new")
-        say_hi = types.InlineKeyboardButton(text="Зачитай реп", callback_data="say_hi")
         keyboard.add(add_new)
-        keyboard.add(say_hi)
         return keyboard
 
     def make_mailing(self, message):
-        users = self.get_users()
-        for user in users:        
-            if int(user) != message.from_user.id:
-                self.bot.send_message(user, message.text)
+        self.bot.send_message(chat_id="@%s" % TELEGRAM['channel_name'], text=message.text)
         self.bot.send_message(message.from_user.id, "Я закончил рассылку\nЧто дальше?",  reply_markup=self.get_main_keyboard())
 
     def send_event(self, event):
