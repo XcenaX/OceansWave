0320f526de50b90ee0da8800ce6f8d145bc99828
XcenaX <vlad-057@mail.ru>
2021-05-18 19:44:57 +0600

    bot update

--- oceanswave/settings.py
+++ oceanswave/settings.py
@@ -17,6 +17,9 @@ from Bot import Bot
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 TELEGRAM = {
     'bot_token': '1895297659:AAFdRK2iPvGAQeOCB8auCqOshuE3VKomenQ',
     'channel_name': 'oceanswavenews',
