bd3d1ba5f760a3bfb15f7dc7f0267a5fd8d0c709
XcenaX <vlad-057@mail.ru>
2021-05-20 22:14:14 +0600

    update bd

--- Bot.py
+++ Bot.py
@@ -15,8 +15,8 @@ class Bot():
     def __init__(self, settings):
         self.bot = telebot.TeleBot(settings["bot_token"])
         self.channel_name = settings["channel_name"]
-        self.owners_file = settings["BASE_DIR"]/self.owners_file
-        self.users_file = settings["BASE_DIR"]/self.users_file
+        #self.owners_file = settings["BASE_DIR"]/self.owners_file
+        #self.users_file = settings["BASE_DIR"]/self.users_file
 
     def send_event(self, event):
         message_html = render_to_string('telegram_message.html', {
