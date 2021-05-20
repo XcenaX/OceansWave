7853ef126a29c17473b84a7ef6d17d397176aea1
XcenaX <vlad-057@mail.ru>
2021-05-20 20:53:37 +0600

    update bd

--- Bot.py
+++ Bot.py
@@ -19,47 +19,6 @@ class Bot():
         self.channel_name = settings["channel_name"]
         self.owners_file = settings["BASE_DIR"]/self.owners_file
         self.users_file = settings["BASE_DIR"]/self.users_file
-         
-
-    def get_owners(self):
-        with open(self.owners_file) as f:
-            content = f.readlines()
-        content = [x.strip() for x in content] 
-        return content
-
-    def get_users(self):
-        with open(self.users_file) as f:
-            content = f.readlines()
-        content = [x.strip() for x in content] 
-        return content
-
-    def add_user(self, id):
-        id = str(id)
-        users = self.get_users()
-        if id not in users:
-            with open(self.users_file, "a") as f:
-                f.write("\n"+id)
-
-    def check_owner(self, name):
-        name = str(name)
-        owners = self.get_owners()
-        if name in owners:
-            return True
-        return False
-
-    def check_date_format(self, date):        
-        try:
-            datetime.datetime.strptime(date, "%Y.%m.%d %H:%M")
-        except:
-            return False
-        return True
-
-
-    def get_main_keyboard(self):
-        keyboard = types.InlineKeyboardMarkup() 
-        add_new = types.InlineKeyboardButton(text="Сделать рассылку", callback_data="add_new")
-        keyboard.add(add_new)
-        return keyboard
 
     def send_event(self, event):
         users = self.get_users()
@@ -67,8 +26,6 @@ class Bot():
             'event': event
         })
         self.bot.send_message(chat_id="@%s" % self.channel_name, text=message_html, parse_mode="html")
-        # for user in users:         
-        #     bot.send_message(user, text=event_text, parse_mode="html")   
         
    
 
